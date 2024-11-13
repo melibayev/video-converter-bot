59b303eaa89d7a0c9c5a62fa796a0ccb06714547
melibayev <melibayefff1@gmail.com>
2024-11-13 17:51:50 +0500

    bugs fixed

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()  # Loads variables from the .env file
-TOKEN = '7657686849:AAE6RoHCMOvFJPaSVxe5I_BmkUnIrxgasP0'
+TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 # Maximum allowed file size (in bytes, for example, 50MB)
 MAX_FILE_SIZE = 50 * 1024 * 1024  # 50 MB
 
