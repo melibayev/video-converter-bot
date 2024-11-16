2b50eefb670906c565715116eb6a6bbcbd1d581a
melibayev <melibayefff1@gmail.com>
2024-11-16 15:52:37 +0500

    env added"

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()  # Loads variables from the .env file
-# TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-TOKEN = '7657686849:AAE6RoHCMOvFJPaSVxe5I_BmkUnIrxgasP0'
+TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+
 # Maximum allowed file size (in bytes, for example, 50MB)
 MAX_FILE_SIZE = 50 * 1024 * 1024  # 50 MB
 
@@ -100,7 +100,6 @@ async def handle_video(update: Update, context):
     try:
         # Convert video to MP3
         ffmpeg.input(video_path).output(output_path).run(cmd='ffmpeg', overwrite_output=True)
-
         # Sending MP3 file to user with bot link in the caption
         await converting_message.delete()
         await update.message.reply_audio(
